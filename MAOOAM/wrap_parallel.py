f0f0b2a6eb5602c6f9188825d5ebee295790ded8
Takuma Yoshida <35924685+ysdtkm@users.noreply.github.com>
2018-04-23 19:44:05 -0400

    modify

--- MAOOAM/wrap_parallel.py
+++ MAOOAM/wrap_parallel.py
@@ -12,7 +12,12 @@ WDIR_BASE = "/home/tak/shrt/parallel_exp"
 
 def shell(cmd):
     assert isinstance(cmd, str)
-    sp.run(cmd, shell=True, check=True)
+    p = sp.run(cmd, shell=True, encoding="utf8", stderr=sp.PIPE, stdout=sp.PIPE)
+    if p.returncode != 0:
+        print(p.stdout)
+        print(p.stderr)
+        raise Exception("shell %s failed" % cmd)
+    return p.stdout, p.stderr
 
 def exec_single_job(param):
     p1, p2 = param
@@ -24,7 +29,8 @@ def exec_single_job(param):
     rewrite_file("analysis_init.py", 104, "sigma_r", "sigma_r = %f" % p1)
     rewrite_file("generate_observations.py", 14, "sigma", "sigma = %f" % p1)
     shell("make")
-    print("%s done")
+    shell("cp -f out.pdf %s/out/%s.pdf" % (WDIR_BASE, get_file_name(p1, p2)))
+    print("%s done" % dname)
     return None
 
 def rewrite_file(filename, linenum, txt_from, txt_to):
@@ -40,6 +46,11 @@ def rewrite_file(filename, linenum, txt_from, txt_to):
 
 def get_dir_name(p1, p2):
     dn = "%s/oerr_%.05f/aint_%05d" % (WDIR_BASE, p1, p2)
+    dn = dn.replace(".", "_")
+    return dn
+
+def get_file_name(p1, p2):
+    dn = "oerr_%.05f_aint_%05d" % (p1, p2)
     dn = dn.replace(".", "_")
     return dn
 
@@ -67,7 +78,7 @@ def main():
     params_prod = itertools.product(params1, params2)
 
     shell("cd %s && make clean" % MODELDIR)
-    shell("rm -rf %s" % WDIR_BASE)
+    shell("rm -rf %s && mkdir -p %s/out" % (WDIR_BASE, WDIR_BASE))
     with Pool(cpu_count()) as p:
         res = p.map(exec_single_job, params_prod)
     res2d = inverse_itertools_2d_product(params1, params2, res)
