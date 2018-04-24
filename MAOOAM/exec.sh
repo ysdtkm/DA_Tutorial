60d73aaa8c62a714746191ebf99fd7abb47baabb
Takuma Yoshida <35924685+ysdtkm@users.noreply.github.com>
2018-04-24 11:18:38 -0400

    modify

--- MAOOAM/exec.sh
+++ MAOOAM/exec.sh
@@ -9,6 +9,7 @@ storagedir="/lustre/tyoshida/data/sync/maooam"
 # preparation
 word=t`/lustre/tyoshida/repos/python/pythonpath/oneliner/serial`
 wdir="${wdir_base}/${word}"
+parallel=true
 
 cd ${modeldir}
 make clean
@@ -16,20 +17,26 @@ git commit -a --allow-empty -m "MAOOAM/fortran exec.sh auto commit: experiment $
 
 echo "preparing files at ${wdir}"
 rm -rf ${wdir}
-mkdir -p ${wdir}/template
+if [ ${parallel} = "true" ]; then
+  mkdir -p ${wdir}/template
+  cp -rf ${modeldir}/* ${wdir}/template/
+else
+  mkdir -p ${wdir}
+  cp -rf ${modeldir}/* ${wdir}/
+fi
 cd ${wdir}
-
-cp -rf ${modeldir}/* ./template
-# cp -rf ${modeldir}/* .
 
 echo "#!/bin/bash"                                    > tmp.sh
 echo "#SBATCH -n 20"                                 >> tmp.sh
 echo "#SBATCH -t 01:15:00"                           >> tmp.sh
 echo "#SBATCH -J ${word}"                            >> tmp.sh
 echo "set -e"                                        >> tmp.sh
-echo "python3 template/wrap_parallel.py ${wdir}"     >> tmp.sh
-echo "cp -r out ${storagedir}/${word}"               >> tmp.sh
-# echo "make all"                                      >> tmp.sh
-# echo "cp out.pdf ${storagedir}/${word}.pdf"          >> tmp.sh
+if [ ${parallel} = "true" ]; then
+  echo "python3 template/wrap_parallel.py ${wdir}"     >> tmp.sh
+  echo "cp -r out ${storagedir}/${word}"               >> tmp.sh
+else
+  echo "make all"                                      >> tmp.sh
+  echo "cp out.pdf ${storagedir}/${word}.pdf"          >> tmp.sh
+fi
 
 sbatch tmp.sh
