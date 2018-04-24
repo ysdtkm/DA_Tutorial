193c7e0c62c8f948aa76c2708495951cb0b6c72a
Takuma Yoshida <35924685+ysdtkm@users.noreply.github.com>
2018-04-24 09:03:25 -0400

    MAOOAM/fortran exec.sh auto commit: experiment t414

--- MAOOAM/exec.sh
+++ MAOOAM/exec.sh
@@ -16,19 +16,19 @@ git commit -a --allow-empty -m "MAOOAM/fortran exec.sh auto commit: experiment $
 
 echo "preparing files at ${wdir}"
 rm -rf ${wdir}
-mkdir -p ${wdir}
+mkdir -p ${wdir}/template
 cd ${wdir}
 
-# cp -rf ${modeldir}/* ./template
-cp -rf ${modeldir}/* .
+cp -rf ${modeldir}/* ./template
+# cp -rf ${modeldir}/* .
 
 echo "#!/bin/bash"                                    > tmp.sh
 echo "#SBATCH -n 20"                                 >> tmp.sh
 echo "#SBATCH -t 01:15:00"                           >> tmp.sh
 echo "#SBATCH -J ${word}"                            >> tmp.sh
 echo "set -e"                                        >> tmp.sh
-# echo "python3 template/wrap_parallel.py ${wdir}"              >> tmp.sh
-echo "make clean all"                                >> tmp.sh
+echo "python3 template/wrap_parallel.py ${wdir}"              >> tmp.sh
+# echo "make clean all"                                >> tmp.sh
 echo "cp out.pdf ${storagedir}/${word}.pdf"          >> tmp.sh
 
 sbatch tmp.sh
