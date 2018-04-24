f735c811b45173e83fe6251a2a17f122b85e1a84
Takuma Yoshida <35924685+ysdtkm@users.noreply.github.com>
2018-04-24 09:19:04 -0400

    MAOOAM/fortran exec.sh auto commit: experiment t417

--- MAOOAM/exec.sh
+++ MAOOAM/exec.sh
@@ -11,7 +11,7 @@ word=t`/lustre/tyoshida/repos/python/pythonpath/oneliner/serial`
 wdir="${wdir_base}/${word}"
 
 cd ${modeldir}
-rm -f *.pkl *.npy
+make clean
 git commit -a --allow-empty -m "MAOOAM/fortran exec.sh auto commit: experiment ${word}"
 
 echo "preparing files at ${wdir}"
@@ -29,7 +29,7 @@ echo "#SBATCH -J ${word}"                            >> tmp.sh
 echo "set -e"                                        >> tmp.sh
 echo "python3 template/wrap_parallel.py ${wdir}"     >> tmp.sh
 echo "cp -r out ${storagedir}/${word}"               >> tmp.sh
-# echo "make clean all"                                >> tmp.sh
+# echo "make all"                                      >> tmp.sh
 # echo "cp out.pdf ${storagedir}/${word}.pdf"          >> tmp.sh
 
 sbatch tmp.sh
