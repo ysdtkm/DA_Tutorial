fa65d2c3b981da871ecd4938e96694213b28724b
Takuma Yoshida <35924685+ysdtkm@users.noreply.github.com>
2018-04-24 11:31:53 -0400

    DA_Tutorial/MAOOAM exec.sh auto commit: experiment t419

--- MAOOAM/wrap_parallel.py
+++ MAOOAM/wrap_parallel.py
@@ -7,7 +7,36 @@ import itertools
 from multiprocessing import Pool, cpu_count
 import numpy as np
 
-_wdir_base = "/home/tak/shrt/parallel_exp"
+class GlobalParams:
+    wdir_base = "/home/tak/shrt/parallel_exp"
+    params1 = [0.1 ** i for i in range(0, 2)]
+    params2 = [10 ** i for i in range(0, 2)]
+
+    def get_wdir_absolute_path(p1, p2):
+        assert p1 in GlobalParams.params1
+        assert p2 in GlobalParams.params2
+        dn = "%s/oerr_%.05f/aint_%05d" % (GlobalParams.wdir_base, p1, p2)
+        dn = dn.replace(".", "_")
+        return dn
+
+    def get_relative_file_name(p1, p2):
+        dn = "oerr_%.05f_aint_%05d" % (p1, p2)
+        dn = dn.replace(".", "_")
+        return dn
+
+    def get_params_prod():
+        return itertools.product(GlobalParams.params1, GlobalParams.params2)
+
+    def inverse_itertools_2d_product(map_result):
+        # return 2D list, un-flattening the map_result
+        try:
+            dummy = iter(map_result)
+        except:
+            raise Exception
+        n1 = len(GlobalParams.params1)
+        n2 = len(GlobalParams.params2)
+        res = [[map_result[i1 * n2 + i2] for i2 in range(n2)] for i1 in range(n1)]
+        return res
 
 def shell(cmd):
     assert isinstance(cmd, str)
@@ -20,15 +49,16 @@ def shell(cmd):
 
 def exec_single_job(param):
     p1, p2 = param
-    dname = get_dir_name(p1, p2)
+    dname = GlobalParams.get_wdir_absolute_path(p1, p2)
     shell("mkdir -p %s" % dname)
     os.chdir(dname)
-    shell("cp -r %s/template/* ." % _wdir_base)
+    shell("cp -r %s/template/* ." % GlobalParams.wdir_base)
     rewrite_file("analysis_init.py", 137, "acyc_step", "acyc_step = %d" % p2)
     rewrite_file("analysis_init.py", 104, "sigma_r", "sigma_r = %f" % p1)
     rewrite_file("generate_observations.py", 14, "sigma", "sigma = %f" % p1)
     shell("make")
-    shell("cp -f out.pdf %s/out/%s.pdf" % (_wdir_base, get_file_name(p1, p2)))
+    shell("cp -f out.pdf %s/out/%s.pdf" %
+        (GlobalParams.wdir_base, GlobalParams.get_relative_file_name(p1, p2)))
     print("%s done" % dname)
     return None
 
@@ -43,48 +73,14 @@ def rewrite_file(filename, linenum, txt_from, txt_to):
             else:
                 f.write(l)
 
-def get_dir_name(p1, p2):
-    dn = "%s/oerr_%.05f/aint_%05d" % (_wdir_base, p1, p2)
-    dn = dn.replace(".", "_")
-    return dn
-
-def get_file_name(p1, p2):
-    dn = "oerr_%.05f_aint_%05d" % (p1, p2)
-    dn = dn.replace(".", "_")
-    return dn
-
-def reduce_mapped_result(params1, params2, map_res):
-    import mydebug
-    import matplotlib
-    matplotlib.use("PDF")
-    import matplotlib.pyplot as plt
-    mydebug.dump_array(np.array(map_res))
-    cm = plt.imshow(map_res)
-    plt.colorbar(cm)
-    plt.savefig("tmp.pdf")
-    plt.close()
-
-def inverse_itertools_2d_product(params1, params2, map_result):
-    # un-flatten an iterable object made by itertools.product
-    n1 = len(params1)
-    n2 = len(params2)
-    res = [[map_result[i1 * n2 + i2] for i2 in range(n2)] for i1 in range(n1)]
-    return res
-
 def main():
-    global _wdir_base
     if len(sys.argv) > 1:
-        _wdir_base = sys.argv[1]
-    shell("mkdir -p %s/out" % _wdir_base)
-
-    params1 = [0.1 ** i for i in range(0, 2)]
-    params2 = [10 ** i for i in range(0, 2)]
-    params_prod = itertools.product(params1, params2)
-
+        GlobalParams.wdir_base = sys.argv[1]
+    shell("mkdir -p %s/out" % GlobalParams.wdir_base)
+    params_prod = GlobalParams.get_params_prod()
     with Pool(cpu_count()) as p:
         res = p.map(exec_single_job, params_prod)
-    res2d = inverse_itertools_2d_product(params1, params2, res)
-    # reduce_mapped_result(params1, params2, res2d)
+    res2d = GlobalParams.inverse_itertools_2d_product(res)
 
 if __name__ == "__main__":
     main()
