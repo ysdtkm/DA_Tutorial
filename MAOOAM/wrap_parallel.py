7abf9e440e048cf9219f28734355a2a76c083089
Takuma Yoshida <35924685+ysdtkm@users.noreply.github.com>
2018-04-30 18:16:55 -0400

    DA_Tutorial/MAOOAM exec.sh auto commit: experiment t0477

--- MAOOAM/wrap_parallel.py
+++ MAOOAM/wrap_parallel.py
@@ -15,19 +15,22 @@ def main():
     shell("mkdir -p %s/out" % wdir_base)
     res = exec_parallel(wdir_base, "template", params1, params2, "ens_%02d", "infl_%.02f",
                   changes1, changes2, "make", "out.pdf")
-    print(list(res))
+    plot_reduced_rmse(params1, params2, res)
 
 def plot_reduced_rmse(params1, params2, res):
     n1, n2 = len(params1), len(params2)
     nr = len(res[0][0])
     res_npy = np.empty((nr, n1, n2))
+    names = ["atmos_psi", "atmos_temp", "ocean_psi", "ocean_temp", "all"]
     for i1 in range(n1):
         for i2 in range(n2):
             res_npy[:, i1, i2] = res[i1][i2]
     for ir in range(nr):
         cm = plt.imshow(res_npy[ir, :, :])
         plt.colorbar(cm)
-        plt.savefig("rmse.pdf")
+        plt.xlabel("inflation rho")
+        plt.ylabel("ensemble member")
+        plt.savefig("rmse_%s.pdf" % names[ir])
         plt.close()
 
 if __name__ == "__main__":
