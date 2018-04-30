c68772147a8308e1963bb302cb91624f45529018
Takuma Yoshida <35924685+ysdtkm@users.noreply.github.com>
2018-04-30 18:57:44 -0400

    DA_Tutorial/MAOOAM exec.sh auto commit: experiment t0483

--- MAOOAM/module_plot.py
+++ MAOOAM/module_plot.py
@@ -111,7 +111,7 @@ def __sample_read_files():
                        "img/freerun.pdf", *vlim_raw, "freerun", "viridis")
     plot_time_colormap(nature.getTrajectory(),
                        "img/nature.pdf", *vlim_raw, "nature", "viridis")
-    for method in ["ETKF"]:
+    for method in ["hybrid"]:
         analysis_file = 'x_analysis_{method}.pkl'.format(method=method)
         das = da_system()
         das = das.load(analysis_file)
