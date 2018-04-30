922355fc6402502c5cd48a0f510d05ded17d86ca
Takuma Yoshida <35924685+ysdtkm@users.noreply.github.com>
2018-04-30 18:11:06 -0400

    DA_Tutorial/MAOOAM exec.sh auto commit: experiment t0476

--- MAOOAM/wrap_parallel.py
+++ MAOOAM/wrap_parallel.py
@@ -2,6 +2,9 @@
 
 import sys
 from util_parallel import Change, shell, exec_parallel
+import matplotlib
+matplotlib.use("PDF")
+import matplotlib.pyplot as plt
 
 def main():
     wdir_base = sys.argv[1]
@@ -13,6 +16,19 @@ def main():
     res = exec_parallel(wdir_base, "template", params1, params2, "ens_%02d", "infl_%.02f",
                   changes1, changes2, "make", "out.pdf")
     print(list(res))
+
+def plot_reduced_rmse(params1, params2, res):
+    n1, n2 = len(params1), len(params2)
+    nr = len(res[0][0])
+    res_npy = np.empty((nr, n1, n2))
+    for i1 in range(n1):
+        for i2 in range(n2):
+            res_npy[:, i1, i2] = res[i1][i2]
+    for ir in range(nr):
+        cm = plt.imshow(res_npy[ir, :, :])
+        plt.colorbar(cm)
+        plt.savefig("rmse.pdf")
+        plt.close()
 
 if __name__ == "__main__":
     main()
