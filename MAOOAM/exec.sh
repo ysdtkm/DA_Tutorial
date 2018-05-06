b96688a711ee881c2ae899a2bb3ad89a3748c632
Takuma Yoshida <35924685+ysdtkm@users.noreply.github.com>
2018-05-06 16:22:14 -0400

    MAOOAM/fortran exec.sh auto commit: experiment t0558

--- MAOOAM/exec.sh
+++ MAOOAM/exec.sh
@@ -22,7 +22,7 @@ cp -rf ${modeldir}/* .
 
 echo "#!/bin/bash"                                    > tmp.sh
 echo "#SBATCH -n 20"                                 >> tmp.sh
-echo "#SBATCH -t 22:15:00"                           >> tmp.sh
+echo "#SBATCH -t 03:15:00"                           >> tmp.sh
 echo "#SBATCH -J ${word}"                            >> tmp.sh
 echo "set -e"                                        >> tmp.sh
 echo "make clean all"                                >> tmp.sh
