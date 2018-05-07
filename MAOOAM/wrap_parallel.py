bafad5b96477b2b8e517be404ec1626f7306ebb7
Takuma Yoshida <35924685+ysdtkm@users.noreply.github.com>
2018-05-07 18:09:45 -0400

    DA_Tutorial/MAOOAM exec.sh auto commit: experiment t0595

--- MAOOAM/wrap_parallel.py
+++ MAOOAM/wrap_parallel.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 
 def main():
     wdir_base = sys.argv[1]
-    params1 = [0.001 * 10 ** (x / 20.0) for x in range(-4, 21)]
+    params1 = [0.001 * 10 ** (x / 20.0) for x in range(0, 21)]
     params2 = [1]
     changes1 = [Change("analysis_init.py", 99, "sigma_b", "sigma_b = %f")]
     changes2 = []
