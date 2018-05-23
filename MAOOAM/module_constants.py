e845c4128744a1cbdf8a920419261358fa4394a7
Takuma Yoshida <35924685+ysdtkm@users.noreply.github.com>
2018-05-22 22:52:32 -0400

    modify

--- MAOOAM/module_constants.py
+++ MAOOAM/module_constants.py
@@ -20,6 +20,7 @@ def get_x_std():
 
 def get_static_b():
     bcov = np.load("binary_const/mean_b_cov_t0644_0001tu.npy")
+    # mean_b_cov_t0645_0010tu.npy
     eigs = np.linalg.eigvals(bcov)
     assert np.all(eigs > 0.0)
     srad = np.max(eigs)
