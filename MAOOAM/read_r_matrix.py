d68963e590399a7d02811eab1ba6b15137fa3022
Takuma Yoshida <35924685+ysdtkm@users.noreply.github.com>
2018-06-19 16:19:18 -0400

    modify

--- MAOOAM/read_r_matrix.py
+++ MAOOAM/read_r_matrix.py
@@ -2,14 +2,20 @@
 
 import numpy as np
 
-def get_r_luyu():
+def get_r_luyu(name):
     n = 36
     r = np.zeros((n, n))
-    with open("binary_const/clim_std_Luyu_20180619.txt", "r") as f:
+    assert name in ["Luyu", "Kriti"]
+    with open(f"binary_const/clim_std_{name}_20180619.txt", "r") as f:
         for i in range(n):
             t = f.readline().strip().replace("D", "E")
             r[i, i] = (float(t) * 0.1) ** 2
     return r
 
 if __name__ == "__main__":
-    print(get_r_luyu())
+    import matplotlib.pyplot as plt
+    plt.plot(np.diag(get_r_luyu("Luyu")), label="Luyu")
+    plt.plot(np.diag(get_r_luyu("Kriti")), label="Kriti")
+    plt.legend()
+    plt.savefig("out.pdf")
+    plt.close()
