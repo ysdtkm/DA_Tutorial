f5400d0ada31bd7c21822a2658d309ff1b4c6703
Takuma Yoshida <35924685+ysdtkm@users.noreply.github.com>
2018-06-29 15:05:32 -0400

    modify

--- MAOOAM/read_r_matrix.py
+++ MAOOAM/read_r_matrix.py
@@ -16,11 +16,30 @@ def get_r_luyu(name):
             r[i, i] = float(t) ** 2
     return r
 
+def get_b_clim_kriti():
+    b = np.load("binary_const/20180629_cov_kriti_1e6tu.npy")
+    assert b.shape == (36, 36)
+    return b
+
 def get_h_b_ht():
     h = get_h()
-    diag_b_10_percent = get_r_luyu("Luyu")
-    hbht = h @ diag_b_10_percent @ h.T
+    b = get_b_clim_kriti()
+    hbht = h @ b @ h.T * 0.01
     return np.diag(np.diag(hbht))
+
+def compare_h_b_ht_diag():
+    h = get_h()
+    b = get_b_clim_kriti()
+    hbht = h @ b @ h.T * 0.01
+    hbhtd = h @ np.diag(np.diag(b)) @ h.T * 0.01
+    plt.title("10% climatology variance stdv at obs location")
+    plt.semilogy(np.diag(hbht) ** 0.5, label="true")
+    plt.semilogy(np.diag(hbhtd) ** 0.5, label="with diagonalized Bclim")
+    plt.xlabel("observation index")
+    plt.ylabel("10% clim variance stdv by m/s or K")
+    plt.legend()
+    plt.savefig("out.pdf", bbox_inches="tight")
+    plt.close()
 
 def print_h_b_ht():
     cm = plt.imshow(get_h_b_ht(), norm=matplotlib.colors.SymLogNorm(linthresh=0.1 ** 8), cmap="RdBu_r")
@@ -39,4 +58,4 @@ def compare_luyu_kriti():
     plt.close()
 
 if __name__ == "__main__":
-    print_h_b_ht()
+    compare_h_b_ht_diag()
