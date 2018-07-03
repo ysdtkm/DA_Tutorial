d071998287d4905447898551e247380cb5bc82cc
Takuma Yoshida <35924685+ysdtkm@users.noreply.github.com>
2018-07-03 12:57:41 -0400

    modify

--- MAOOAM/maooam/test_aotensor.py
+++ MAOOAM/maooam/test_aotensor.py
@@ -1,11 +1,15 @@
+import unittest
 import aotensor as aotensor_mod
 
-aotensor,Li,Lj,Lk,Lv=aotensor_mod.init_aotensor()
+class TestAoTensor(unittest.TestCase):
+    @unittest.skipIf(True, "skip model test")
+    def test_aotensor(self):
+        aotensor,Li,Lj,Lk,Lv=aotensor_mod.init_aotensor()
 
-real_eps = 2.2204460492503131e-16
+        real_eps = 2.2204460492503131e-16
 
-"""This module print the Ocean-Atmosphere tendencies tensor"""
+        """This module print the Ocean-Atmosphere tendencies tensor"""
 
-for x in aotensor:
-	print("aotensor["+str(x[0])+"]"+"["+str(x[1])+"]"+"["+str(x[2])+"]"+" = % .5E" % x[3])
+        for x in aotensor:
+            print("aotensor["+str(x[0])+"]"+"["+str(x[1])+"]"+"["+str(x[2])+"]"+" = % .5E" % x[3])
 
