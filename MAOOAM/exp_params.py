1edaba2c015f87c93bec158311d5d169eec55c7f
Takuma Yoshida <35924685+ysdtkm@users.noreply.github.com>
2018-07-06 15:25:33 -0400

    fail at step 16

--- MAOOAM/exp_params.py
+++ MAOOAM/exp_params.py
@@ -5,7 +5,7 @@ SEED = PRIME * 2
 X0_INIT = "Cheng"
 
 T_RUN = 10 ** 4
-DT = 0.1
+DT = 0.5
 ACYC_STEP = int(2.5 / DT)
 assert abs(DT * ACYC_STEP - 2.5) < 1.0e-6
 FLAG_R = "Cheng"
@@ -17,7 +17,7 @@ RHO = 1.0
 
 BCOV_FROM = "Cheng"
 TDVAR_METHOD = "oi"
-SIGMA_B = 0.0002
+SIGMA_B = 0.005
 DIAGONALIZE_B = False
 
 SKIP_HEAVY_PLOT = True
