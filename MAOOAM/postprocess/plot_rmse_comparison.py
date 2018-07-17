882efa9939dc249c33b6e844ec4d4507c12585d7
Takuma Yoshida <35924685+ysdtkm@users.noreply.github.com>
2018-07-17 13:47:13 -0400

    modify

--- MAOOAM/postprocess/plot_rmse_comparison.py
+++ MAOOAM/postprocess/plot_rmse_comparison.py
@@ -7,38 +7,27 @@ import matplotlib
 matplotlib.use("pdf")
 import matplotlib.pyplot as plt
 
+DIR_RESULTS = "/lustre/tyoshida/six_month/result_exempt"
+
 def read_error_ETKF_or_hybrid(parent_dir, expname):
     with open(f"{parent_dir}/{expname}/plot_reduced_rmse.pkl", "rb") as f:
         obj = pickle.load(f)
     return obj
 
+def read_error_3DVar(parent_dir, expname):
+    obj = np.load(f"{parent_dir}/{expname}/rmse_3DVar.npy")
+    return obj
+
 class TestAll(unittest.TestCase):
     def test_read_error_ETKF_or_hybrid(self):
-        parent_dir = "/lustre/tyoshida/six_month/result_exempt"
-        exp = "t0900"
-        print(read_error_ETKF_or_hybrid(parent_dir, exp))
+        for exp in ["t0900", "t0901", "t0902", "t0931", "t0932", "t0933"]:
+            res = read_error_ETKF_or_hybrid(DIR_RESULTS, exp)
+            self.assertTrue(isinstance(res, list))
 
-"""[tyoshida@login-1:/lustre/tyoshida/six_month/result_exempt ]
-$ find .
-.
-./t0902
-./t0902/plot_reduced_rmse.pkl
-./t0945
-./t0945/rmse_3DVar.npy
-./t0900
-./t0900/plot_reduced_rmse.pkl
-./t0932
-./t0932/plot_reduced_rmse.pkl
-./t0901
-./t0901/plot_reduced_rmse.pkl
-./t0942
-./t0942/rmse_3DVar.npy
-./t0944
-./t0944/rmse_3DVar.npy
-./t0931
-./t0931/plot_reduced_rmse.pkl
-./t0933
-./t0933/plot_reduced_rmse.pkl"""
+    def test_read_error_3DVar(self):
+        for exp in ["t0942", "t0944", "t0945"]:
+            res = read_error_3DVar(DIR_RESULTS, exp)
+            self.assertTrue(isinstance(res, np.ndarray))
 
 if __name__ == "__main__":
     unittest.main()
