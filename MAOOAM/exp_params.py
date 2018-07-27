ec98b82bf2dc668322a09197d47da69b1876fef0
Takuma Yoshida <35924685+ysdtkm@users.noreply.github.com>
2018-07-27 11:57:50 -0400

    modify

--- MAOOAM/exp_params.py
+++ MAOOAM/exp_params.py
@@ -10,7 +10,7 @@ T_RUN = 10 ** 5
 DT = 0.1
 ACYC_STEP = int(round(2.5 / DT))
 ERROR_FREE_OBS = False
-OBS_NET = "ocn_grid"
+OBS_NET = "full_grid"
 FLAG_R = "hbht"
 
 EDIM = 37
---
+++ MAOOAM/interactive.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import matplotlib
+matplotlib.use("tkagg")
+import matplotlib.pyplot as plt
+import numpy as np
+import module_obs_network
+import IPython
+
+def plot_r():
+    # h = module_obs_network.get_h()
+    h = module_obs_network.read_fort_txt_2d("binary_const/20180727_kriti_h.txt").T
+    bclim = np.load("binary_const/20180629_cov_kriti_1e6tu.npy")
+
+    luyu_r = module_obs_network.read_fort_txt_2d("binary_const/clim_std_Luyu_20180619.txt").flatten()
+
+    kriti_r_obs_space = module_obs_network.read_fort_txt_2d("binary_const/20180727_kriti_r_in_obs_space.txt").flatten() ** 2
+    kriti_hxxThT = module_obs_network.read_fort_txt_2d("fort.502").flatten() ** 2
+
+    r_with_diag = np.diag(h @ np.diag(luyu_r ** 2) @ h.T)
+    r_no_diag = np.diag(h @ bclim * 0.01 @ h.T)
+
+    plt.semilogy(r_with_diag, label="Luyu's R (diagonal)")
+    plt.semilogy(r_no_diag, label="hxxThT, mine")
+    plt.semilogy(kriti_r_obs_space, label="kriti hrrhT")
+    plt.semilogy(kriti_hxxThT, label="kriti_hxxThT")
+    plt.legend()
+    plt.show()
+
+    IPython.embed()
+
+def get_kriti_r():
+    h = module_obs_network.read_fort_txt_2d("binary_const/20180727_kriti_h.txt")
+    ma = np.max(np.abs(h))
+    norm = matplotlib.colors.SymLogNorm(linthresh=ma * 0.1 ** 16)
+    cm = plt.imshow(h, norm=norm, cmap="RdBu_r")
+    plt.colorbar(cm)
+    plt.show()
+    IPython.embed()
+
+plot_r()
